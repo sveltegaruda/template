c5b8c21985298b394a19a1cd16f2d236cbd537be
Zen <muhzaini30@gmail.com>
2020-10-28 22:49:40 +0800

    Perbaikan script purge css

--- script/pakaiPurge.js
+++ script/pakaiPurge.js
@@ -1,12 +1,27 @@
 fs = require('fs')
 var recursive = require("recursive-readdir");
 const { PurgeCSS } = require('purgecss')
+
+/*
+abaikan: css yang nggak kena pengaruh purge
+safelist: rule css yang nggak kena purge
+*/
+abaikan = ['global.css'] // bisa juga: asset/halo.css (nama foldernya ikutkan)
+safelist = []
+
 recursive("public/dist", (_, x) => {
-	x = x.filter(x => x.match(/\.css/g)).filter(x => x != "global.css")
+	x = x.filter(x => x.match(/\.css/g)).filter(x => {
+		for (y of abaikan){
+			if (x == `public/dist/${y}`) {
+				return false
+			}
+		}
+		return true
+	})
 	new PurgeCSS().purge({
-	  content: ['src/**/*.svelte'],
+	  content: ['src/**/*.svelte', 'public/index.html'],
 	  css: [...x],
-	  safelist: ['body', '.dropdown-menu-show']
+	  safelist: [...safelist]
 	}).then(x => {
 		for (y of x){
 			fs.writeFile(y.file.replace('public/', 'hasil/'), y.css, 'utf8', () => {})
